8e01d4a0fc3f7bfdf2d651e47aee9b31f91b07e2
Phillip Curl <phillipcurl@gmail.com>
2019-09-13 15:44:29 -0400

    Resolving path issue created when moving content to assets

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const siteInfo = require('./content/setup/config.json');
+const siteInfo = require('./assets/content/setup/config.json');
 console.log(siteInfo);
 const glob = require('glob');
 const path = require('path');
@@ -7,102 +7,102 @@ const path = require('path');
 // automatically and match it to the path of your Nuxt routes.
 // The Nuxt routes are generate by Nuxt automatically based on the pages folder.
 const dynamicRoutes = getDynamicPaths({
-  // '/blog': 'blog/posts/*.json',
+	// '/blog': 'blog/posts/*.json',
 });
 
 export default {
-  mode: 'universal',
+	mode: 'universal',
 
-  head: {
-    lang: 'en',
-    title: siteInfo.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: siteInfo.description
-      }
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
-  },
+	head: {
+		lang: 'en',
+		title: siteInfo.name,
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				hid: 'description',
+				name: 'description',
+				content: siteInfo.description
+			}
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+	},
 
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: { color: '#fff' },
 
-  /*
-   ** Global CSS
-   */
-  css: [],
+	/*
+	 ** Global CSS
+	 */
+	css: [],
 
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: ['@nuxtjs/pwa', '@nuxtjs/markdownit', '@bazzite/nuxt-netlify'],
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: [],
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: ['@nuxtjs/pwa', '@nuxtjs/markdownit', '@bazzite/nuxt-netlify'],
 
-  markdownit: {
-    injected: true,
-    preset: 'default',
-    breaks: true,
-    html: true
-  },
+	markdownit: {
+		injected: true,
+		preset: 'default',
+		breaks: true,
+		html: true
+	},
 
-  pwa: {
-    icon: {
-      iconSrc: `./static/${siteInfo.icon}`
-      // iconFileName: siteInfo.icon.replace(/^.*[\\\/]/, '')
-    },
-    manifest: {
-      name: siteInfo.name,
-      short_name: siteInfo.name,
-      description: siteInfo.description,
-      lang: 'en'
-    },
-    workbox: {
-      runtimeCaching: [
-        {
-          urlPattern: '/images/uploads/.*',
-          handler: 'cacheFirst',
-          strategyOptions: {
-            cacheName: 'image-cache',
-            cacheExpiration: {
-              maxEntries: 100,
-              maxAgeSeconds: 86400
-            }
-          }
-        }
-      ]
-    }
-  },
+	pwa: {
+		icon: {
+			iconSrc: `./static/${siteInfo.icon}`
+			// iconFileName: siteInfo.icon.replace(/^.*[\\\/]/, '')
+		},
+		manifest: {
+			name: siteInfo.name,
+			short_name: siteInfo.name,
+			description: siteInfo.description,
+			lang: 'en'
+		},
+		workbox: {
+			runtimeCaching: [
+				{
+					urlPattern: '/images/uploads/.*',
+					handler: 'cacheFirst',
+					strategyOptions: {
+						cacheName: 'image-cache',
+						cacheExpiration: {
+							maxEntries: 100,
+							maxAgeSeconds: 86400
+						}
+					}
+				}
+			]
+		}
+	},
 
-  /*
-   ** Route config for pre-rendering
-   */
-  router: {},
+	/*
+	 ** Route config for pre-rendering
+	 */
+	router: {},
 
-  generate: {
-    fallback: true,
-    routes: dynamicRoutes
-  },
+	generate: {
+		fallback: true,
+		routes: dynamicRoutes
+	},
 
-  plugins: [],
+	plugins: [],
 
-  /*
-   ** Build configuration
-   */
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {}
-  }
+	/*
+	 ** Build configuration
+	 */
+	build: {
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) {}
+	}
 };
 
 /**
@@ -110,13 +110,13 @@ export default {
  * @param {*} urlFilepathTable
  */
 function getDynamicPaths(urlFilepathTable) {
-  // if (process.argv.includes('--spa')) return [];
-  return [].concat(
-    ...Object.keys(urlFilepathTable).map(url => {
-      const filepathGlob = urlFilepathTable[url];
-      return glob
-        .sync(filepathGlob, { cwd: 'content' })
-        .map(filepath => `${url}/${path.basename(filepath, '.json')}/`);
-    })
-  );
+	// if (process.argv.includes('--spa')) return [];
+	return [].concat(
+		...Object.keys(urlFilepathTable).map(url => {
+			const filepathGlob = urlFilepathTable[url];
+			return glob
+				.sync(filepathGlob, { cwd: 'content' })
+				.map(filepath => `${url}/${path.basename(filepath, '.json')}/`);
+		})
+	);
 }
