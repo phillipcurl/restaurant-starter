d63134b37cf41fd131d9d736ab4801cdb521e66a
Phillip Curl <phillipcurl@gmail.com>
2019-09-16 14:22:03 -0400

    Adding price filter to menu item

--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -2,14 +2,14 @@
 	<article class="w-full md:w-1/2 p-4 mb-2">
 		<header class="w-full flex items-end">
 			<h3
-				class="flex-shrink-0 text-xl font-semibold leading-tight pr-3"
+				class="text-xl font-semibold leading-tight pr-3"
 				:class="{'uppercase tracking-wide': themeInfo.theme === 'modern'}"
 			>{{item.title}}</h3>
 			<div
-				class="flex-grow mb-1 border-b-2"
+				class="flex-grow mb-1 md:border-b-2"
 				:class="{'border-dotted': themeInfo.theme === 'modern', 'border-solid': themeInfo.theme === 'classic'}"
 			></div>
-			<span class="flex-shrink-0 block pl-3 pb-1 leading-none">{{item.price}}</span>
+			<span class="flex-shrink-0 block pl-3 pb-1 leading-none">{{item.price | price}}</span>
 		</header>
 		<div class="mt-2">
 			<p class="lh-copy">{{item.description}}</p>
@@ -35,6 +35,12 @@ export default {
 		},
 		menu() {
 			return this.$store.state.menu.fullMenu;
+		}
+	},
+	filters: {
+		// Filter definitions
+		price(value) {
+			return `$${value.toFixed(2)}`;
 		}
 	}
 };
